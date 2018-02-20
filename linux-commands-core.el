2404ca5aaeb650c6bb3b3371641429fc1205d0fe
DongSoo Lee <dongsoolee8@gmail.com>
2018-02-20 21:25:23 +0900

    Change Emacs Lisp file names

---
+++ linux-commands-core.el
@@ -0,0 +1,14 @@
+;;; core.el --- Linux Commands core
+
+;; Copyright (C) 2018 Dongsoo Lee
+
+;; Author: Dongsoo Lee <mrlee603@gmail.com>
+;; Maintainer: Dongsoo Lee <mrlee603@gmail.com>
+;; Created: 2018-02-20 21:18:00
+;; 
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
