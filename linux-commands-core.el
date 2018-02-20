cb4cb9cc1048640b365a2e53bae459c78b9610fe
DongSoo Lee <dongsoolee8@gmail.com>
2018-02-20 21:43:27 +0900

    Test working the contents cache when publish pages

--- linux-commands-core.el
+++ linux-commands-core.el
@@ -12,3 +12,25 @@
 ;; 
 
 ;;; Code:
+
+(defvar lc-core/current-contents nil)
+(defvar lc-core/current-contents-filename nil)
+(defun lc-core/init-contents (filename)
+  (setq filename (expand-file-name filename))
+  (ignore-errors
+	(setq lc-core/current-contents
+		  (with-temp-buffer
+			(insert-file-contents filename)
+			(org-mode)
+			(org-element-parse-buffer)
+			))
+	(setq lc-core/current-contents-filename filename))
+  )
+
+(defun lc-core/get-current-contents ()
+  (when (and
+		 (stringp lc-core/current-contents-filename)
+		 (equal lc-core/current-contents-filename buffer-file-name))
+	lc-core/current-contents))
+
+(provide 'linux-commands-core)
