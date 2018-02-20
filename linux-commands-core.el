51cd8592b92d55952712d016ff526e62f232c8a9
mrlee23 <mrlee_23@naver.com>
2018-02-20 17:02:28 +0000

    Publish-en: Fix again from 11e800b

--- linux-commands-core.el
+++ linux-commands-core.el
@@ -13,8 +13,19 @@
 
 ;;; Code:
 
+(defvar lc-core/url "http://linux-command.org")
+(defvar lc-core/site-name "@@LANG_EN:Linux Commands@@@@LANG_ES:Comandos de Linux@@@@LANG_KO:리눅스 명령어@@@@LANG_ZH:Linux命令@@@@LANG_JA:Linuxコマンド@@")
+
+(defvar lc-core/base-url nil)
+(defvar lc-core/language nil)
+
 (defvar lc-core/current-contents nil)
 (defvar lc-core/current-contents-filename nil)
+
+(defun lc-core/init-lang (lang)
+  (setq lc-core/language (format "%s" lang))
+  (setq lc-core/base-url (format "%s/%s" lc-core/url lc-core/language)))
+
 (defun lc-core/init-contents (filename)
   (setq filename (expand-file-name filename))
   (ignore-errors
@@ -24,8 +35,8 @@
 			(org-mode)
 			(org-element-parse-buffer)
 			))
-	(setq lc-core/current-contents-filename filename))
-  )
+	(setq lc-core/current-contents-filename filename)
+	))
 
 (defun lc-core/get-current-contents ()
   (when (and
